44eb58e7692a60ffed4404471ee85c2e0c88a09a
Francisco Santos <fdossantosleite@apside.pt>
2023-07-07 16:46:06 +0100

    Development branch - Changed the login and create user process

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,13 +8,21 @@ import{
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
+
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor){
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor (private dto: ClassConstructor) {}
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
         return handler.handle().pipe(
-            map((data: any) => {
-                return plainToInstance(UserDto, data, {
+            map((data: ClassConstructor) => {
+                return plainToInstance(this.dto, data, {
                     excludeExtraneousValues: true,
                 });
             }),
